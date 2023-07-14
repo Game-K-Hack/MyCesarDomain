2b38b6463060b9358d50318e488c5abcc654b4bc
Game K <hack.h@laposte.net>
2023-07-14 15:39:41 +0200

    update addons

--- background.js
+++ background.js
@@ -2,62 +2,46 @@ let currentTab;
 let copystatus;
 
 function updateIcon(tabs) {
-  browser.browserAction.setIcon({
-    path: copystatus ? {
-      16: "icons/check-button-16.png",
-      24: "icons/check-button-24.png",
-      32: "icons/check-button-32.png",
-      64: "icons/check-button-64.png",
-      128: "icons/check-button-128.png",
-      256: "icons/check-button-256.png",
-      512: "icons/check-button-512.png"
-    } : {
-      16: "icons/laurier-16.png",
-      24: "icons/laurier-24.png",
-      32: "icons/laurier-32.png",
-      64: "icons/laurier-64.png",
-      128: "icons/laurier-128.png",
-      256: "icons/laurier-256.png",
-      512: "icons/laurier-512.png"
-    },
-    tabId: tabs.id
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    if (tabs && tabs.length > 0) {
+      chrome.browserAction.setIcon({
+        path: copystatus ? {
+          16: "icons/check-button-16.png",
+          24: "icons/check-button-24.png",
+          32: "icons/check-button-32.png",
+          64: "icons/check-button-64.png",
+          128: "icons/check-button-128.png",
+          256: "icons/check-button-256.png",
+          512: "icons/check-button-512.png"
+        } : {
+          16: "icons/laurier-16.png",
+          24: "icons/laurier-24.png",
+          32: "icons/laurier-32.png",
+          64: "icons/laurier-64.png",
+          128: "icons/laurier-128.png",
+          256: "icons/laurier-256.png",
+          512: "icons/laurier-512.png"
+        },
+        tabId: tabs.id
+      });
+    }
   });
-}
-
-function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
-  
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
-
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
-
-  try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
-  }
-
-  document.body.removeChild(textArea);
 }
 
 function copyTextToClipboard(text) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
-  }
-  navigator.clipboard.writeText(text).then(function() {
-    console.log('Async: Copying to clipboard was successful!');
-  }, function(err) {
-    console.error('Async: Could not copy text: ', err);
-  });
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+
+  textArea.select();
+  textArea.setSelectionRange(0, textArea.value.length);
+
+  document.execCommand("copy");
+
+  document.body.removeChild(textArea);
+  console.log("Async: Copying to clipboard was successful!");
 }
+
 
 function Ceasar(word, offset) {
   let newArray = [];
@@ -93,12 +77,12 @@ function toggleBookmark(tabs) {
 
 }
 
-browser.browserAction.onClicked.addListener(toggleBookmark);
+chrome.browserAction.onClicked.addListener(toggleBookmark);
 // listen to tab URL changes
-browser.tabs.onUpdated.addListener(updateIcon);
+chrome.tabs.onUpdated.addListener(updateIcon);
 // listen to tab switching
-browser.tabs.onActivated.addListener(updateIcon);
+chrome.tabs.onActivated.addListener(updateIcon);
 // listen for window switching
-browser.windows.onFocusChanged.addListener(updateIcon);
+chrome.windows.onFocusChanged.addListener(updateIcon);
 
 updateIcon();
